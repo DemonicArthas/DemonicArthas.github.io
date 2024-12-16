955469b58c263e44c3c8ac55614c2df115e1c991
Nikita Tulin <ntulin.work@gmail.com>
2024-12-16 17:05:11 -0300

    Configure eslint & prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,16 @@
 export default {
   plugins: ['prettier-plugin-tailwindcss'],
-}
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  printWidth: 120,
+  tabWidth: 2,
+  overrides: [
+    {
+      files: ['tsconfig.json'],
+      options: {
+        trailingComma: 'none',
+      },
+    },
+  ],
+};
