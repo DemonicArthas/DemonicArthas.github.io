b86903dd31e7e8b8487ab82930bf7c83a5de1acf
Nikita Tulin <ntulin.work@gmail.com>
2024-12-18 20:06:34 -0300

    Change site origin

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -12,7 +12,7 @@ export default extendConfig(baseConfig, () => {
     },
     plugins: [
       staticAdapter({
-        origin: "https://demonicarthas.github.io/portfolio",
+        origin: "https://demonicarthas.github.io",
       }),
     ],
   };
