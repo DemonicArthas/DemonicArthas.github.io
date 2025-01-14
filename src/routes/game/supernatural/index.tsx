70fb370a2c303613628abe41faea7d7073e0f5ca
DemonicArthas <thedemonicarthas@gmail.com>
2025-01-14 20:18:14 +0300

    Footage for supernatural

--- src/routes/game/supernatural/index.tsx
+++ src/routes/game/supernatural/index.tsx
@@ -8,6 +8,10 @@ export default component$(() => {
         <h2>Supernatural: The Game</h2>
         <p>Supernatural The Game is a short prequel to the famous TV-series</p>
       </article>
+      <video width="1024" height="768" controls autoplay muted>
+        <source src={`/games/supernatural/footage.mp4`} type="video/mp4">
+      Your browser does not support the video tag.
+      </video> 
     </div>
   );
 });
