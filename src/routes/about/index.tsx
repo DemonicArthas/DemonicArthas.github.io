b74a99ec1d245d94edf3381b7d0e35cb13e5c575
DemonicArthas <thedemonicarthas@gmail.com>
2025-01-14 19:00:27 +0300

    Bugfix with mixed spaced and tabs

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -9,8 +9,6 @@ export default component$(() => {
         <p>
           My name is Albert Leonov and I'm a technical game designer. I love designing and implementing game mechanics,
           creating levels or just messing around in Unreal Engine 4 and 5.
-		</p>
-		<p>
 		  I can create a prototype for you from scratch, design and develop a new game mechanic for your project or consult on anything and everything concerning Unreal!
         </p>
       </article>
