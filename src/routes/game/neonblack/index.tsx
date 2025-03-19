5e4229ecf6dd46143d968dc4e5846ea8f2887771
DemonicArthas <thedemonicarthas@gmail.com>
2025-03-19 12:41:47 +0300

    Bugfixes (mixed spaces and tabs)

--- src/routes/game/neonblack/index.tsx
+++ src/routes/game/neonblack/index.tsx
@@ -5,17 +5,17 @@ export default component$(() => {
   return (
     <div class="bg-base-200 p-2">
       <article class="prose p-4 lg:prose-lg">
-	  	  <h2>Neon Black</h2>
-		    <p>3D stealth-action platformer. Avoid the gaze of "The Boss", take down guards and collect various usable items to aid you in your quest.</p>
-		    <p>Team project where I was fully responsible for game design and level design decisions.</p>
+      <h2>Neon Black</h2>
+        <p>3D stealth-action platformer. Avoid the gaze of "The Boss", take down guards and collect various usable items to aid you in your quest.</p>
+        <p>Team project where I was fully responsible for game design and level design decisions.</p>
       </article>
       <video width="1024" height="768" controls muted>
         <source src="/games/neonblack/NeonBlack.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-		  <a href="https://slayerdf.github.io/NeonBlack/">Play Neon Black in browser (Github.io)</a>
+      <a href="https://slayerdf.github.io/NeonBlack/">Play Neon Black in browser (Github.io)</a>
       <br></br>
-	  </div>
+    </div>
   );
 });
 
